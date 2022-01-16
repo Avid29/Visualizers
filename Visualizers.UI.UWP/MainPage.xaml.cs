6540e14e8323a92c206f1b60ec620a3faf4456c3
Adam Dernis <avid29@live.com>
2022-01-15 23:30:08 -0500

    Implemeted shader runner

--- Visualizers.UI.UWP/MainPage.xaml.cs
+++ Visualizers.UI.UWP/MainPage.xaml.cs
@@ -1,35 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Visualizers.UI.Shared.Audio.Rendering;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Visualizers.UI.Shared.Render;
 using Windows.Storage.Pickers;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Visualizers.UI.UWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private AudioGraphRenderer _audioRenderer;
+        private VisualizerRunner _visualizerRunner;
 
         public MainPage()
         {
             this.InitializeComponent();
 
+            _visualizerRunner = new VisualizerRunner();
             Init();
         }
 
@@ -37,11 +23,14 @@ namespace Visualizers.UI.UWP
         {
             _audioRenderer = new AudioGraphRenderer();
             await _audioRenderer.InitializeAsync();
+            _visualizerRunner.AudioRenderer = _audioRenderer;
 
             FileOpenPicker filePicker = new FileOpenPicker();
             filePicker.FileTypeFilter.Add(".mp3");
             var file = await filePicker.PickSingleFileAsync();
             _audioRenderer.PlayFile(file);
         }
+
+        public VisualizerRunner VisualizerRunner => _visualizerRunner;
     }
 }
