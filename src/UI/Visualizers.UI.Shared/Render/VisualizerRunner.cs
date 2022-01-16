10cbccf7611cde8292b2ca8c4a627fe2cca088ea
Adam Dernis <avid29@live.com>
2022-01-15 23:29:45 -0500

    Created Visualizer runner

---
+++ src/UI/Visualizers.UI.Shared/Render/VisualizerRunner.cs
@@ -0,0 +1,38 @@
+﻿using ComputeSharp;
+using ComputeSharp.Uwp;
+using System;
+using Visualizers.UI.Shared.Audio.Rendering;
+
+namespace Visualizers.UI.Shared.Render
+{
+    public class VisualizerRunner : IShaderRunner
+    {
+        private ReadOnlyBuffer<float> _audioBuffer;
+        private AudioGraphRenderer _audioRenderer;
+
+        public AudioGraphRenderer AudioRenderer
+        {
+            get => _audioRenderer;
+            set
+            {
+                if (_audioRenderer != null) _audioRenderer.FramePlayed -= ReadFrame;
+
+                _audioRenderer = value;
+                _audioRenderer.FramePlayed += ReadFrame;
+            }
+        }
+
+        public bool TryExecute(IReadWriteTexture2D<Float4> texture, TimeSpan timespan, object parameter)
+        {
+            if (_audioBuffer == null) return false;
+
+            GraphicsDevice.Default.ForEach(texture, new TestShader(_audioBuffer));
+            return true;
+        }
+
+        private void ReadFrame(object sender, float[] e)
+        {
+            if (e.Length > 0) _audioBuffer = GraphicsDevice.Default.AllocateReadOnlyBuffer(e);
+        }
+    }
+}
